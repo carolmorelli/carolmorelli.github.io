a4a9a1c19c80dcc2a3c3c170d40217b331f413f5
carolmorelli <carolmorelli@gmail.com>
2023-06-06 20:12:15 -0300

    ajus

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230607.070458';
+const cacheName = 'chirpy-20230607.071135';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
