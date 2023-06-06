1f9828fb725e87154e4b48a4ac015ee9b858f8af
carolmorelli <carolmorelli@gmail.com>
2023-06-06 20:05:30 -0300

    ajuste

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230607.070221';
+const cacheName = 'chirpy-20230607.070458';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
