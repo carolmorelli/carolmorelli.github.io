7dc70526a87d32393816300269608fcbcbe53fee
carolmorelli <carolmorelli@gmail.com>
2023-06-06 22:18:21 -0300

    finalizado_animal

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230607.075439';
+const cacheName = 'chirpy-20230607.091548';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
