8284a41e5cf0e901bb8884028c80039e77a8783c
carolmorelli <carolmorelli@gmail.com>
2023-06-11 18:58:43 -0300

    ajuste titulo

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230612.052342';
+const cacheName = 'chirpy-20230612.055736';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
