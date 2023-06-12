55f15c8feaec482da85b62cb4e4a1539bc221786
carolmorelli <carolmorelli@gmail.com>
2023-06-12 05:51:21 -0300

    update_animal

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230612.055736';
+const cacheName = 'chirpy-20230612.064828';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
