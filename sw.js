29f0b304ff6034bcbde9a28e9fe9bd8392bd536a
carolmorelli <carolmorelli@gmail.com>
2023-06-19 21:18:18 -0300

    inicio magic

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230620.032204';
+const cacheName = 'chirpy-20230620.081401';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
