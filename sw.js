9f6ffb51ec34d0a6cab52c4a659ef23b2fce34e4
carolmorelli <carolmorelli@gmail.com>
2023-06-26 15:04:19 -0300

    previa magic

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230620.081401';
+const cacheName = 'chirpy-20230627.020324';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
